ca4d4487c50300758dba813382e90b65b5485447
@stpunk47 <stpunk47@gmail.com>
2025-02-24 23:35:44 +0200

    Initial commit

---
+++ src/main/java/com/stpunk47/reactive_mongo/ReactiveMongoApplication.java
@@ -0,0 +1,13 @@
+package com.stpunk47.reactive_mongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveMongoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveMongoApplication.class, args);
+	}
+
+}
