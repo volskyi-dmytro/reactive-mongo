ff0c2e8649609ca386c8ab96e9e790fcf6189f0a
@stpunk47 <stpunk47@gmail.com>
2025-02-26 23:00:25 +0200

    Add Mongo Database and Client configuration.

--- src/main/java/com/stpunk47/reactive_mongo/ReactiveMongoApplication.java
+++ src/main/java/com/stpunk47/reactive_mongo/ReactiveMongoApplication.java
@@ -2,8 +2,9 @@ package com.stpunk47.reactive_mongo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {MongoReactiveAutoConfiguration.class})
 public class ReactiveMongoApplication {
 
 	public static void main(String[] args) {
