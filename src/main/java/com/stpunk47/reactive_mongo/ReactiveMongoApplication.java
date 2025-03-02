198f08d41a6350bf197a964f007cbef16040f3bf
@stpunk47 <stpunk47@gmail.com>
2025-03-02 21:04:23 +0200

    Update database configuration.

--- src/main/java/com/stpunk47/reactive_mongo/ReactiveMongoApplication.java
+++ src/main/java/com/stpunk47/reactive_mongo/ReactiveMongoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.mongo.MongoReactiveAutoConfiguration;
 
-@SpringBootApplication(exclude = {MongoReactiveAutoConfiguration.class})
+@SpringBootApplication
 public class ReactiveMongoApplication {
 
 	public static void main(String[] args) {
